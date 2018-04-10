5a8be7938a9b36644e1d7e2fc0b3c50a7c53cbe9
hieupm2096 <hieupm2096@users.noreply.github.com>
2018-04-11 01:32:44 +0700

    On Progress: Implement full version  - On Progress: Login

--- ExamOnline-ejb/src/java/facade/UserFacade.java
+++ ExamOnline-ejb/src/java/facade/UserFacade.java
@@ -8,7 +8,10 @@ package facade;
 import entity.User;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
@@ -33,7 +36,15 @@ public class UserFacade extends AbstractFacade<User> {
         User user = null;
         if (email != null && password != null) {
             String findUserByEmailAndPassword = "SELECT u FROM User u WHERE u.email = :email AND u.password = :password";
-            
+            try {
+                user = (User) em.createQuery(findUserByEmailAndPassword)
+                        .setParameter("email", email)
+                        .setParameter("password", password)
+                        .getSingleResult();
+            } catch (NoResultException e) {
+                Logger logger = LoggerFactory.getLogger(UserFacade.class);
+                logger.error(e.getMessage());
+            }
         }
         // else  email or/and password is empty
         return user;
