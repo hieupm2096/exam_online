d53cf884303abc36e35481a9d39df71c9ba2636a
hieupm2096 <hieupm2096@users.noreply.github.com>
2018-04-11 03:37:29 +0700

    On Progress: Implement full version  - On Progress: Login  - Done: Simple Login / no Authorize

--- ExamOnline-war/src/java/controller/AuthenticationBean.java
+++ ExamOnline-war/src/java/controller/AuthenticationBean.java
@@ -5,25 +5,37 @@
  */
 package controller;
 
+import entity.User;
 import facade.UserFacade;
 import java.io.Serializable;
 import javax.ejb.EJB;
 import javax.inject.Named;
-import javax.enterprise.context.Dependent;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
  * @author oswal
  */
 @Named(value = "authenticationBean")
-@Dependent
-public class AuthenticationBean implements Serializable{
+@SessionScoped
+public class AuthenticationBean implements Serializable {
 
     @EJB
     private UserFacade userFacade;
 
+    // logger
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthenticationBean.class);
+
+    public static final String HOME_PAGE_REDIRECT = "/app/home?faces-redirect=true";
+    public static final String LOGIN_PAGE_REDIRECT = "/login?faces-redirect=true";
+
     private String email;
     private String password;
+
+    private User loginUser;
 
     public String getEmail() {
         return email;
@@ -40,11 +52,41 @@ public class AuthenticationBean implements Serializable{
     public void setPassword(String password) {
         this.password = password;
     }
-    
+
     public AuthenticationBean() {
     }
-    
-    public void login() {
+
+    public String login() {
+        loginUser = userFacade.findUserByEmailAndPassword(email, password);
         
+        if (loginUser != null) {
+            LOGGER.info("login successfully for '{}'", loginUser.getName());
+            return HOME_PAGE_REDIRECT;
+        } else {
+            LOGGER.info("login failed");
+            return null;
+        }
+    }
+    
+    public String logout() {
+        String identifier = loginUser.getName();
+        
+        // invalidate session
+        LOGGER.debug("invalidating session for ", identifier);
+        FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+        
+        LOGGER.info("logout successfully for ", identifier);
+        return LOGIN_PAGE_REDIRECT;
+    }
+    
+    public boolean isLoggedIn() {
+        return loginUser != null;
+    }
+    
+    public String isLoggedInForwardHome() {
+        if (isLoggedIn()) {
+            return HOME_PAGE_REDIRECT;
+        }
+        return null;
     }
 }
